1535b4f0cfc6b03e43b2ec91b5e04bc56d85f29c
xueyuans <shi.xuey@husky.neu.edu>
2018-03-12 10:20:28 -0700

    update

--- server.js
+++ server.js
@@ -1,38 +1,49 @@
-var express = require('express');
-const bodyParser = require('body-parser');
-const app = express();
+
+// Get the dependencies
+
+const express = require('express');
 const path = require('path');
 const http = require('http');
+const bodyParser = require('body-parser');
+const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
 
-//CORS
-app.use(function(reg, res, next){
+
+
+
+// Point static path to dist -- For building -- REMOVE
+app.use(express.static(path.join(__dirname, 'dist')));
+
+
+
+// CORS
+app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE, OPTIONS');
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   next();
-})
+});
 
-const port=process.env.PORT || '3100';
+
+
+
+const port = process.env.PORT || '5000';
 app.set('port', port);
+
 
 // Create HTTP server
 const server = http.createServer(app);
 
+
+
+/*
 // For Build: Catch all other routes and return the index file -- BUILDING
-//app.get('*', function (req, res) {
-//  res.sendFile(path.join(__dirname, 'dist/index.html'));
-//});
+app.get('*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'dist/index.html'));
+});
+*/
 
 
-var hello = require('./assignment/app');
-hello(app);
-
-
-// require("./app")(app);
-
-server.listen( port , function() {
-  console.log('Node app is running on port', app.get('port'))});
+server.listen( port , () => console.log('Running on port 5000'));
