367765f48302ec8d04cc97d4c244ca9e4f4f7fba
xueyuans <sxyshixueyuan@gmail.com>
2018-03-16 10:42:58 -0700

    Update website.service.client.ts

--- src/app/services/website.service.client.ts
+++ src/app/services/website.service.client.ts
@@ -2,7 +2,7 @@ import { Website } from '../models/website.model.client';
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
 import 'rxjs/Rx';
-import { environment } from '../../environments/environment';
+import { environment } from '../../environments/environment.prod';
 
 @Injectable()
 export class WebsiteService {
