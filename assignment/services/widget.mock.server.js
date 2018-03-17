dd9d5a5ab71c3bc9419e9f29a6ec1f5733417166
xueyuans <shi.xuey@husky.neu.edu>
2018-03-17 01:06:34 -0700

    update

--- assignment/services/widget.mock.server.js
+++ assignment/services/widget.mock.server.js
@@ -1,9 +1,9 @@
 var WIDGETS = [
-  // { "_id": "123", "widgetType": "HEADER",  "pageId": "321", "size": 1, "text": "GIZMODO"},
-  // { "_id": "234", "widgetType": "HEADER", "pageId": "321", "size": 2, "text": "Lorem ipsum"},
-  // { "_id": "345", "widgetType": "IMAGE", "pageId": "321", "size": 2, "text": "testing 3", "width": "100%", "url": "http://lorempixel.com/400/200/"},
-  // { "_id": "567", "widgetType": "HTML", "pageId": "321", "size": 2, "text": "<p>blalbla</p>"},
-  // { "_id": "456", "widgetType": "YOUTUBE", "pageId": "321", "size": 2, "text": "testing 4", "width": "100%", "url": "//www.youtube.com/embed/ZpbgzR-2FXo"}
+  { "_id": "123", "widgetType": "HEADER",  "pageId": "321", "size": 1, "text": "GIZMODO"},
+  { "_id": "234", "widgetType": "HEADER", "pageId": "321", "size": 2, "text": "Lorem ipsum"},
+  { "_id": "345", "widgetType": "IMAGE", "pageId": "321", "size": 2, "text": "testing 3", "width": "100%", "url": "http://lorempixel.com/400/200/"},
+  { "_id": "567", "widgetType": "HTML", "pageId": "321", "size": 2, "text": "<p>blalbla</p>"},
+  { "_id": "456", "widgetType": "YOUTUBE", "pageId": "321", "size": 2, "text": "testing 4", "width": "100%", "url": "//www.youtube.com/embed/ZpbgzR-2FXo"}
 ];
 
 module.exports = WIDGETS;
