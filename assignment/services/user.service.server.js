4c03d094731944fd833f8df63261b6d5d548d28b
xueyuans <shi.xuey@husky.neu.edu>
2018-04-09 10:58:57 -0700

    1.3

--- assignment/services/user.service.server.js
+++ assignment/services/user.service.server.js
@@ -137,6 +137,7 @@ module.exports = function (app) {
             var names = profile.displayName.split(" ");
             var newFacebookUser = {
               username: profile.displayName,
+              password: profile.displayName,
               lastName:  names[1],
               firstName: names[0],
               email:     profile.emails ? profile.emails[0].value:"",
