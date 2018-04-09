25a1549a85f7f993b6df8c4204f6299b71a2030e
xueyuans <sxyshixueyuan@gmail.com>
2018-04-09 10:35:55 -0700

    Update model.js

--- assignment/models/model.js
+++ assignment/models/model.js
@@ -1,4 +1,4 @@
 var mongoose = require('mongoose');
-var db = mongoose.connect('mongodb://localhost:27017/webdev');
-// var db = mongoose.connect('mongodb://root:sxy3319081@ds263707.mlab.com:63707/heroku_cwb0ncxv');
+// var db = mongoose.connect('mongodb://localhost:27017/webdev');
+var db = mongoose.connect('mongodb://root:sxy3319081@ds263707.mlab.com:63707/heroku_cwb0ncxv');
 module.exports = db;
